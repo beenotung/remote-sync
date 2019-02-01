e3fce35e44ae69ea65b47d88b33fb577085c35ea
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-02-01 11:28:24 +0800

    added npm bin

--- src/cli.ts
+++ src/cli.ts
@@ -9,34 +9,35 @@ function help(code = 0) {
   process.exit(code);
 }
 
-
-if (process.argv.length < 3) {
-  help(0);
-}
-let mode = process.argv[2];
-switch (mode) {
-  case 'help':
+export function main() {
+  if (process.argv.length < 3) {
     help(0);
-    break;
-  case 'server':
-    if (process.argv.length < 5) {
-      console.log('Error: missing arguments!');
-      help(1);
+  }
+  let mode = process.argv[2];
+  switch (mode) {
+    case 'help':
+      help(0);
       break;
-    }
-    wrapConsoleLog(genLogFilename('server.out'), genLogFilename('server.err'));
-    main_server(process.argv[3], process.argv[4]);
-    break;
-  case 'client':
-    if (process.argv.length < 6) {
-      console.log('Error: missing arguments!');
-      help(1);
+    case 'server':
+      if (process.argv.length < 5) {
+        console.log('Error: missing arguments!');
+        help(1);
+        break;
+      }
+      wrapConsoleLog(genLogFilename('server.out'), genLogFilename('server.err'));
+      main_server(process.argv[3], process.argv[4]);
       break;
-    }
-    wrapConsoleLog(genLogFilename('client.out'), genLogFilename('client.err'));
-    main_client(process.argv[3], process.argv[4], process.argv[5]);
-    break;
-  default:
-    console.log('Error: invalid mode!');
-    help(1);
+    case 'client':
+      if (process.argv.length < 6) {
+        console.log('Error: missing arguments!');
+        help(1);
+        break;
+      }
+      wrapConsoleLog(genLogFilename('client.out'), genLogFilename('client.err'));
+      main_client(process.argv[3], process.argv[4], process.argv[5]);
+      break;
+    default:
+      console.log('Error: invalid mode!');
+      help(1);
+  }
 }
